906ed624cfff7c0bb1d5cc72bec1cb88beec6d56
terran-zealot <vikinglaurasun@gmail.com>
2025-01-29 23:33:38 +0200

    fixin orphography

--- js/task-2.js
+++ js/task-2.js
@@ -1,5 +1,5 @@
 function formatMessage(message, maxLength) {
-    if (message.lenght <= maxLength) {
+    if (message.maxLength <= maxLength) {
         return message;
     } else {
         return message.slice(0, maxLength) + "...";
